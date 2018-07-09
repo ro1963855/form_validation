6ae329b483940ed01bb4f70d03f553cc2e22c899
Dandy.Chen <ro1963855@gmail.com>
2018-07-09 23:59:45 +0800

    init CreateAccount layout and finish FormTitle module

---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fv_crateAccount container">
+    <form-title :title="title" :subtitle="subtitle"></form-title>
+  </div>
+</template>
+
+<script>
+import FormTitle from '@/components/FormTitle';
+
+export default {
+  name: 'fv_crateAccount',
+  components: {
+    FormTitle,
+  },
+  data() {
+    return {
+      title: 'Create Account',
+      subtitle: 'Glad to see you here!',
+    };
+  },
+};
+</script>
---
+++ src/components/FormTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fv_formTitle">
+      <h1>{{ title }}</h1>
+      <h2>{{ subtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fv_formTitle',
+  props: ['title', 'subtitle'],
+  data() {
+    return {};
+  },
+};
+</script>
