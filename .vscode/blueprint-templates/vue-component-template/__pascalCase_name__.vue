ba1e3106be2cb6e28201367dd1aee4c53e568f0b
Dandy <dandy.chen@advantech.com.tw>
2018-07-11 17:15:16 +0800

    add buleprint setting files

---
+++ .vscode/blueprint-templates/vue-component-template/__pascalCase_name__.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="{{ camelCase name }}">
+  </div>
+</template>
+
+<script>
+export default {
+  name: '{{ camelCase name }}',
+  data() {
+    return {};
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+    test() {
+    },
+  },
+};
+</script>
